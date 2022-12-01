116c576455e9a03841d31eee4fed724fc48dc945
dkschdcks <113900719+dkschdcks@users.noreply.github.com>
2022-12-01 17:49:40 +0900

    test

---
+++ src/main/java/com/example/accproject/AccProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.accproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accproject/AccProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
